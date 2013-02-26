85a0519870c288fb7fc988f0e7e8dc3314f5ec37
Geoff Hulette <ghulette@gmail.com>
2013-02-26 12:39:36 -0800

    Cleanup

--- readfile.ml
+++ readfile.ml
@@ -1,11 +1,10 @@
-(* Making OCaml look like Ruby. *)
-
 open Disguise
 
 let counter = ref 1;;
+
 open_in "readfile.ml" >>> fun line -> begin
   print (string_of_int !counter);
   print ": ";
   puts line;
   counter := !counter + 1
-end;;
+end
--- readfile.rb
+++ readfile.rb
@@ -1,7 +1,5 @@
-# To build/run:
-#   $ ruby readfile.rb
-
 counter = 1
+
 File.open("readfile.rb", "r").each_line do |line|
   print counter
   print ": "
