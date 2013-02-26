8f35bc381ed4abee689673bc9c5a09eca0f1150a
Geoff Hulette <ghulette@gmail.com>
2013-02-26 14:33:47 -0800

    Update

--- readfile.ml
+++ readfile.ml
@@ -3,7 +3,7 @@ open Disguise
 let counter = ref 1;;
 
 open_in "readfile.ml" >>> fun line -> begin
-  print (string_of_int !counter);
+  print_int !counter;
   print ": ";
   puts line;
   counter := !counter + 1
