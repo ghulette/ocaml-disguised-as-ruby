75b2b808ecdcec8c4b30613fe0496a369d46bc9f
Geoff Hulette <ghulette@gmail.com>
2013-02-26 12:59:20 -0800

    Update

--- disguise.ml
+++ disguise.ml
@@ -1,6 +1,6 @@
 (* Some functions to make OCaml look like Ruby. *)
 
-let rec each_line ch f = begin
+let rec each_line ch f = 
   try 
     let line = input_line ch in
     f line; 
@@ -8,7 +8,7 @@ let rec each_line ch f = begin
   with
     | End_of_file -> close_in ch
     | _ -> prerr_endline "Error!"
-end;;
+;;
 
 let (>>>) = each_line;;
 let print = print_string;;
